b801da09df3fd38f4452b3dafee22e20a685e12d
Dušan Živković <dusanbass6@gmail.com>
2021-05-12 19:14:24 +0200

    index.ts commented a task

--- index.ts
+++ index.ts
@@ -6,3 +6,11 @@ console.log(`small ${small}`);
 const newMessage:string = "Hello New World!";
 console.log(`NEW BIG: ${newMessage.toUpperCase()}`); // string template
 
+/**
+// initialize TS number
+const newNumer:??? = ???;
+console.log(`newNumer: ${newNumer}`); // string template
+// initialize TS boolean
+const newBoolean:??? = ???;
+console.log(`newBoolean: ${newBoolean}`); // string template
+* */
