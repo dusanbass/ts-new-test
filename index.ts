62ceff37c1d851a3a09019ea816b8d7b811f835f
Dušan Živković <dusanbass6@gmail.com>
2021-05-12 19:11:44 +0200

    index.ts has more examples

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
-const message = "Hello World!";
+const message: string = "Hello World!";
 const small = message.toLowerCase();
 console.log(`small ${small}`);
+
+// for primitives its not that important...
+const newMessage = "Hello New World!";
+console.log(`NEW BIG: ${newMessage.toUpperCase()}`); // string template
