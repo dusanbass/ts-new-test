e1602e2e51d813011b9543ea76bae7e5ae7ea9a4
Dušan Živković <dusanbass6@gmail.com>
2021-05-12 19:12:26 +0200

    index.ts

--- index.ts
+++ index.ts
@@ -3,5 +3,6 @@ const small = message.toLowerCase();
 console.log(`small ${small}`);
 
 // for primitives its not that important...
-const newMessage = "Hello New World!";
+const newMessage:string = "Hello New World!";
 console.log(`NEW BIG: ${newMessage.toUpperCase()}`); // string template
+
