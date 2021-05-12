6e3da4d983c9150eed66b2b97d0773169e824be6
Dušan Živković <dusanbass6@gmail.com>
2021-05-12 21:09:34 +0200

    final class update

--- index.ts
+++ index.ts
@@ -3,16 +3,80 @@ const small = message.toLowerCase();
 console.log(`small ${small}`);
 
 // for primitives its not that important...
-const newMessage:string = "Hello New World!";
+const newMessage: string = "Hello New World!";
 console.log(`NEW BIG: ${newMessage.toUpperCase()}`); // string template
 
-/**
+
 // initialize TS number
-const newNumer:??? = ???;
+const newNumer: number = 5;
 console.log(`newNumer: ${newNumer}`); // string template
 // initialize TS boolean
-const newBoolean:??? = ???;
+const newBoolean: boolean = false;
 console.log(`newBoolean: ${newBoolean}`); // string template
 
-// now run it and see if it appears in the console!
-* */
+// now run it 'npm run start' and see if it appears in the console!
+/**
+small hello world!
+NEW BIG: HELLO NEW WORLD!
+newNumer: 5
+newBoolean: false
+*/
+
+
+// Arithmetic Operators
+
+let x = 0;
+console.log(`x = 0; ${x}`)
+x = x + newNumer; // = 5
+console.log(`x = x + newNumer; ${newNumer}`)
+x += newNumer; // = 10
+console.log(`x += newNumer; ${newNumer}`)
+// const notANum = '5';
+// x = x + notANum; // Type 'string' is not assignable to type 'number'.ts(2322)
+x = x++;
+console.log(`x++; ${newNumer}`)
+x = x--;
+console.log(`x--; ${newNumer}`)
+
+const xGtNewNum = x > newNumer;
+console.log(`xGtNewNum; ${xGtNewNum}`);
+
+// Functions
+// internet example: function function_name (param1[:type], param2?[:type]): returnType
+//                   KEYWORD  <arbitrary>   (parameter [:optionalParamType], parameterOptional2? [:string]): typeOfReturn
+function ourLoggerFunction(a: string, b?: string): void { console.log('ourLoggerFunction', a, b) }
+// function ourLoggerFunctionVanilla (a, b) {}
+ourLoggerFunction('log this');
+ourLoggerFunction('log and this', 'optional that');
+
+function mySumFunction(a: number = 0, b: number = 0): number { return a + b }
+const result0 = mySumFunction(1,2);
+console.log(`mySumFunction(1,2); ${result0}`);
+const result1 = mySumFunction(1);
+console.log(`mySumFunction(1); ${result1}`);
+
+// Arrays
+// const arr = [1, 2, 3]; // vanilla js
+const arr: Array<number> = [1, 2, 3]; // TypeScript
+// const arr: number[] = [1, 2, 3]; // TypeScript, this is valid too!!!
+
+// Tuple != Array, looks the same tho
+const tup: Array<string|number> = [1, 'number two', 3];
+// const tup: (string|number)[] = [1, 'number two', 3];
+console.log(`tup; ${tup}`);
+tup.pop();
+console.log(`tup.pop(); ${tup}`);
+
+// Interfaces
+//keyword nam { param: type; nextParam: nextType }
+interface Cat {
+    age: number;
+    name: string;
+    // meow: () => void
+}
+
+const cat: Cat = {
+    age: 5,
+    name: 'Black'
+}
+
