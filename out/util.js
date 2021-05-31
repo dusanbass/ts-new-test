31e1b7a4933dd46b1cda678ab7ed5db7aa26415e
Dušan Živković <dusanbass6@gmail.com>
2021-05-31 17:14:26 +0200

    updated project to have a functional debugger, check the readme

---
+++ out/util.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hi = void 0;
+function hi() { console.log('hi'); }
+exports.hi = hi;
+//# sourceMappingURL=util.js.map
