f0720659b6cecabfa16261ad6f1deb45cdcd2c71
Dušan Živković <dusanbass6@gmail.com>
2021-05-31 19:35:54 +0200

    new Point3D class with extends update

--- Point3D.ts
+++ Point3D.ts
@@ -2,7 +2,7 @@ import { Point2D } from "./Point2D";
 
 export default class Point3D extends Point2D {
     z: number;
-    constructor(x: number, y: number, z: number) {
+    constructor(x: number = 0, y: number = 0, z: number = 0) {
         super(x, y); // new Point2D(x, y)
         this.z = z;
     };
