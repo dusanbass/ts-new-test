7b0208fbcea1ea5c7c0033bb7d6a5382d2b178db
Dušan Živković <dusanbass6@gmail.com>
2021-06-09 15:42:46 +0200

    feature xlsx with updated async await

--- ExcelReader.ts
+++ ExcelReader.ts
@@ -2,7 +2,9 @@ import {readFile, utils} from 'xlsx';
 import {promisify} from 'util';
 import {writeFile} from 'fs';
 
-const readExcel = () => {
+const writeLogger = promisify(writeFile);
+
+const  readExcel = async () => {
     const data = readFile('./namesages.xls')
     console.log(data);
     const cats = data.Sheets.Cats;
@@ -10,8 +12,11 @@ const readExcel = () => {
     const dogs = data.Sheets.Dogs;
     console.log(utils.sheet_to_json(dogs));
 
-    const writeLogger = promisify(writeFile);
+    await writeLogger('./loggerOfXLSX_cats.json', JSON.stringify(cats));
+    console.log('cats are converted to JSON too!!')
+
     writeLogger('./loggerOfXLSX.json', JSON.stringify(dogs)).then(() => console.log('written!!!'))
+    console.log('this is the end of readExcel function!')
   } 
 
 export default readExcel;
