751db646aeefd6836d918a6ea170442790cd7e1d
Dušan Živković <dusanbass6@gmail.com>
2021-06-09 15:36:15 +0200

    feature xlsx converts dogs sheet to json

--- ExcelReader.ts
+++ ExcelReader.ts
@@ -1,4 +1,6 @@
-import {readFile, utils} from 'xlsx'
+import {readFile, utils} from 'xlsx';
+import {promisify} from 'util';
+import {writeFile} from 'fs';
 
 const readExcel = () => {
     const data = readFile('./namesages.xls')
@@ -7,6 +9,9 @@ const readExcel = () => {
     console.log(utils.sheet_to_json(cats));
     const dogs = data.Sheets.Dogs;
     console.log(utils.sheet_to_json(dogs));
+
+    const writeLogger = promisify(writeFile);
+    writeLogger('./loggerOfXLSX.json', JSON.stringify(dogs)).then(() => console.log('written!!!'))
   } 
 
 export default readExcel;
