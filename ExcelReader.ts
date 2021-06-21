d482888ab90082b22dee317966b0f57accaa922e
Dušan Živković <dusanbass6@gmail.com>
2021-06-21 21:12:53 +0200

    updated for converting to an array

--- ExcelReader.ts
+++ ExcelReader.ts
@@ -1,22 +1,25 @@
-import {readFile, utils} from 'xlsx';
-import {promisify} from 'util';
-import {writeFile} from 'fs';
+import { readFile, utils, WorkSheet } from 'xlsx';
+import { promisify } from 'util';
+import { writeFile } from 'fs';
 
 const writeLogger = promisify(writeFile);
 
-const  readExcel = async () => {
-    const data = readFile('./namesages.xls')
-    console.log(data);
-    const cats = data.Sheets.Cats;
-    console.log(utils.sheet_to_json(cats));
-    const dogs = data.Sheets.Dogs;
-    console.log(utils.sheet_to_json(dogs));
+const readExcel = async () => {
+  const data = readFile('./namesages.xls')
+  console.log(data);
+  const cats: WorkSheet = data.Sheets.Cats;
+  const catsArray = <{ name: string; year: number }[]>utils.sheet_to_json(cats)
+  console.log(catsArray.map(e => ({nameAge: `${Number(e.year)} + ${e.name}`})));
 
-    await writeLogger('./loggerOfXLSX_cats.json', JSON.stringify(cats));
-    console.log('cats are converted to JSON too!!')
+  console.log(utils.sheet_to_json(cats));
+  const dogs = data.Sheets.Dogs;
+  console.log(utils.sheet_to_json(dogs));
 
-    writeLogger('./loggerOfXLSX.json', JSON.stringify(dogs)).then(() => console.log('written!!!'))
-    console.log('this is the end of readExcel function!')
-  } 
+  await writeLogger('./loggerOfXLSX_cats.json', JSON.stringify(cats));
+  console.log('cats are converted to JSON too!!')
+
+  writeLogger('./loggerOfXLSX.json', JSON.stringify(dogs)).then(() => console.log('written!!!'))
+  console.log('this is the end of readExcel function!')
+}
 
 export default readExcel;
